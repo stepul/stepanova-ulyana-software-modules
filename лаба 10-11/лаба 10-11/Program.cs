9e2090ed32c7fe33d82bee93a1f63f92446cbaff
Ulyana <step.ul13@yandex.ru>
2026-09-25 13:52:42 +0500

    oh
    rgerg

---
+++ лаба 10-11/лаба 10-11/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Console.WriteLine("Введите x1:");
+string input1 = Console.ReadLine();
+double x1 = Convert.ToDouble(input1);
+Console.WriteLine("Введите x2:");
+string input2 = Console.ReadLine();
+double x2 = Convert.ToDouble(input2);
+Console.WriteLine("Введите y1:");
+string input3 = Console.ReadLine();
+double y1 = Convert.ToDouble(input3);
+Console.WriteLine("Введите y2:");
+string input4 = Console.ReadLine();
+double y2 = Convert.ToDouble(input4);
+double dlina1 = Math.Sqrt(x1 * x1 + y1 * y1);
+double dlina2 = Math.Sqrt(x2 * x2 + y2 * y2);
+Console.WriteLine($"Длина1: {dlina1}");
+Console.WriteLine($"Длина2: {dlina2}");
+x1 /= dlina1;
+y1 /= dlina1;
+x2/= dlina2;
+y2/= dlina2;
+double rast1 = x1 - x2;
+double rast2 = y1 - y2;
+double distan = Math.Sqrt(rast1 * rast1 + rast2 * rast2);*/
+
+/*string input1 = Console.ReadLine();
+double x1 = Convert.ToDouble(input1);
+Console.WriteLine("Введите w1:");
+string input2 = Console.ReadLine();
+double w1 = Convert.ToDouble(input2);
+Console.WriteLine("Введите x2:");
+string input3 = Console.ReadLine();
+double x2 = Convert.ToDouble(input3);
+Console.WriteLine("Введите w2:");
+string input4 = Console.ReadLine();
+double w2 = Convert.ToDouble(input4);
+Console.WriteLine("Введите b:");
+string input5 = Console.ReadLine();
+double b = Convert.ToDouble(input5);
+
+double z = x1 * w1 + x2 * w2 + b;
+Console.WriteLine(z);
+
+double e = Math.E;
+double tanh = Math.Tanh((Math.Pow(e, z) - Math.Pow(e, -z)) / (Math.Pow(e, z) +Math.Pow(e, -z)));
+Console.WriteLine($"Гиперболический тангенс: {tanh}");*/
+
+/*Console.WriteLine("Введите N:");
+string input1 = Console.ReadLine();
+double N = Convert.ToDouble(input1);
+Console.WriteLine("Введите D:");
+string input2 = Console.ReadLine();
+double D = Convert.ToDouble(input2);
+Console.WriteLine("Введите K:");
+string input3 = Console.ReadLine();
+double K = Convert.ToDouble(input3);
+double Operations = N * D * K;
+double Million = (N * D * K) / 1000000;
+double Billion = (N * D * K) / 1000000000;
+Console.WriteLine($"Операции: {Operations}");
+Console.WriteLine($"Миллион: {Million}");
+Console.WriteLine($"Миллиард: {Billion}");*/
